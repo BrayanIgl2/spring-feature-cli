5502640b8c3b5b7c63df7dab8a99531a7267d4bf
BrayanIgl2 <iglesiasbrayan2@gmail.com>
2026-04-28 18:27:25 +0200

    refactor: move logic to actions, simplify command

---
+++ src/actions/generateActions.js
@@ -0,0 +1,32 @@
+
+import { runValidations } from '../utils/runValidations.js';
+import { logger } from '../utils/logger.js';
+import fs from 'fs';
+import path from 'path';
+import validations from '../validations/generateValidations.js';
+
+
+export function generateFeature(name) {
+
+    const basePath = process.cwd();
+
+    runValidations(validations, name);
+
+    const structure = {
+        entity: `${name}.java`,
+        service: `${name}Service.java`,
+        controller: `${name}Controller.java`,
+    };
+
+    for (const [folder, file] of Object.entries(structure)) {
+        const folderPath = path.join(basePath, name, folder);
+
+        logger.info(`Creating ${folder}...`);
+        fs.mkdirSync(folderPath, { recursive: true });
+        fs.writeFileSync(path.join(folderPath, file), '');
+    }
+
+    logger.success(`\n` + 'Feature created successfully');
+
+
+}
--- src/commands/generate.js
+++ src/commands/generate.js
@@ -1,12 +1,9 @@
 import { Command } from 'commander';
-import { runValidations } from '../utils/runValidations.js';
+import { generateFeature } from '../actions/generateActions.js';
 import { logger } from '../utils/logger.js';
-import fs from 'fs';
-import path from 'path';
-import validations from '../validations/generateValidations.js';
 
 const generate = new Command('generate');
-const basePath = process.cwd();
+
 
 generate
     .description('Create a new feature (entity, service, controller)')
@@ -14,32 +11,14 @@ generate
     .argument('<featureName>')
     .action((featureName) => {
         try {
+
             const name = featureName?.trim();
+            generateFeature(name);
 
-            runValidations(validations, name);
-
-            const structure = {
-                entity: `${name}.java`,
-                service: `${name}Service.java`,
-                controller: `${name}Controller.java`,
-            };
-
-
-            Object.entries(structure).map(([folder, file]) => {
-                const folderPath = path.join(basePath, name, folder);
-
-                logger.info(`Creating ${folder}...`);
-                fs.mkdirSync(folderPath, { recursive: true });
-                fs.writeFileSync(path.join(folderPath, file), '');
-            })
-
-
-            logger.success(`\n` + 'Feature created successfully');
         } catch (error) {
             logger.error(error.message);
             process.exit(1);
         }
-
     });
 
 export default generate;
