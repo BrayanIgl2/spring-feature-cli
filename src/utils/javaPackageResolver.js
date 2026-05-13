a54c4d077b87bfc3d5819740badece0550d2c7d4
BrayanIgl2 <iglesiasbrayan2@gmail.com>
2026-05-13 02:52:22 +0200

    feat: implement getRootPackage function to extract root package from main class file

--- src/utils/javaPackageResolver.js
+++ src/utils/javaPackageResolver.js
@@ -3,5 +3,16 @@ import fs from 'fs';
 import path from 'path';
 
 export function getMainClassDirectory() {
-return path.dirname(getMainClassFile());
+    return path.dirname(getMainClassFile());
+}
+
+export function getRootPackage() {
+    const file = getMainClassFile();
+    const content = fs.readFileSync(file, 'utf-8');
+
+    const packageLine = content.split('\n').find(line => line.trim().startsWith('package'));
+
+    if (!packageLine) throw new Error("Root Package not found");
+
+    return packageLine.replace('package', '').replace(';', '').trim();
 }
